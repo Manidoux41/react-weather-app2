2fd84a3161d6bd195d338b356cfd5903d30ef73c
Manidoux41 <lherissondu41@gmail.com>
2023-03-25 12:25:43 +0100

    first commit

---
+++ src/components/TemperatureAndDetails.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
+import { FaTemperatureHigh } from "react-icons/fa";
+import { FiWind, FiSunset } from "react-icons/fi";
+import { BsSun } from "react-icons/bs";
+import { WiHumidity } from "react-icons/wi";
+
+function TemperatureAndDetails() {
+  return (
+    <div>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>Cloudy or whatever</p>
+      </div>
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt=""
+          className="w-20"
+        />
+        <p className="text-5xl">34°</p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <FaTemperatureHigh size={18} className="mr-1" />
+            Real fell:
+            <span className="font-medium ml-1">32°</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <WiHumidity size={18} className="mr-1" />
+            Humidity:
+            <span className="font-medium ml-1">65%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <FiWind size={18} className="mr-1" />
+            Wind speed:
+            <span className="font-medium ml-1">11 km/h</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TemperatureAndDetails;
